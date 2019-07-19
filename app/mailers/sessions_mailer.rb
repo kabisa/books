9fa186cbc7e9c884c5f2128bcc387c081e922c4e
Patrick Baselier <patrick.baselier@gmail.com>
2019-07-19 13:11:54 +0200

    Improve mail text

--- app/mailers/sessions_mailer.rb
+++ app/mailers/sessions_mailer.rb
@@ -6,13 +6,13 @@ class SessionsMailer < ApplicationMailer
   #   en.sessions_mailer.magic_link.subject
   #
   def magic_link(user)
-    @user       = user.decorate
+    @user       = user.decorate.to_label
     @magic_link = token_sign_in_url(user.login_token)
     @app_name   = Rails.application.config.app_name
 
     mail to: user.email,
       subject: default_i18n_subject(
-        user: @user.to_label,
+        user: @user,
         app_name: @app_name
     )
   end
