cbf7e5f96fe34e09a65f2270e1e6ac51030cc958
Patrick Baselier <patrick.baselier@gmail.com>
2019-07-25 11:29:07 +0200

    Improve form

---
+++ config/initializers/extensions/simple_form/form_builder.rb
@@ -0,0 +1,24 @@
+# See: https://gist.github.com/maxivak/57a2fb71afeb9efcf771
+module ButtonComponents
+  # @example
+  #   = f.button :submit_cancel
+  #   = f.button :submit_cancel, 'Save'
+  #   = f.button :submit_cancel, cancel: posts_path
+  #   = f.button :submit_cancel, 'Save', cancel: posts_path
+  def submit_cancel(*args, &block)
+    template.content_tag :div, class: 'form-actions' do
+      options = args.extract_options!
+      options[:class] = [options[:class], 'btn-primary'].compact
+      args << options
+
+      # with cancel link
+      if cancel = options.delete(:cancel)
+        submit(*args, &block) + '&nbsp;&nbsp;'.html_safe + template.link_to(I18n.t('simple_form.buttons.cancel'), cancel, class: 'btn btn-flat', role: :button)
+      else
+        submit(*args, &block)
+      end
+    end
+  end
+end
+
+SimpleForm::FormBuilder.include(ButtonComponents)
---
+++ config/initializers/simple_form_daemonite.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Use this setup block to configure all options available in SimpleForm.
+SimpleForm.setup do |config|
+  # vertical forms
+  #
+  # vertical default_wrapper
+  config.wrappers :vertical_form_w_floating_label, tag: 'div', class: 'form-group floating-label', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label
+    b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+    b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+  end
+
+  # The default wrapper to be used by the FormBuilder.
+  config.default_wrapper = :vertical_form_w_floating_label
+end
