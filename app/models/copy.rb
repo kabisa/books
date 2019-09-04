cc89149093e52836862e54736b16a4fd9eb2ee65
Patrick Baselier <patrick.baselier@gmail.com>
2019-09-04 12:45:02 +0200

    Validate `Copy`

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -1,4 +1,6 @@
 class Copy < ApplicationRecord
   belongs_to :book
   belongs_to :location
+
+  validates :number,  numericality: { greater_than: 0, only_integer: true }
 end
--- spec/models/copy_spec.rb
+++ spec/models/copy_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Copy, type: :model do
     it { is_expected.to belong_to(:book) }
     it { is_expected.to belong_to(:location) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_numericality_of(:number).
+         is_greater_than(0).
+         only_integer }
+  end
 end
