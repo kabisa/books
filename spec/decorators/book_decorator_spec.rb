c0c273c7123d2a7708039e0c6b0df7f906b1ac0c
Patrick Baselier <patrick.baselier@gmail.com>
2019-09-09 13:11:41 +0200

    Add more tests

--- spec/decorators/book_decorator_spec.rb
+++ spec/decorators/book_decorator_spec.rb
@@ -11,10 +11,18 @@ RSpec.describe BookDecorator do
       it { is_expected.to eql('E-book') }
     end
 
-    describe 'e-book' do
+    describe 'printed book' do
       let(:book) { build :printed_book }
 
       it { is_expected.to eql('Printed book') }
     end
+  end
+
+  describe '#dom_id' do
+    subject { decorator.dom_id }
+    let(:decorator) { described_class.new(book) }
+    let(:book) { build :ebook }
+
+    it { is_expected.to eql('new_ebook') }
   end
 end
--- spec/decorators/printed_book_decorator_spec.rb
+++ spec/decorators/printed_book_decorator_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe PrintedBookDecorator do
   describe '#available_copies' do
-    before          { allow(instance).to receive(:copies_count).and_return(5) }
+    before          do
+      allow(instance).to receive(:copies_count).and_return(5)
+      allow(instance).to receive(:borrowings_count).and_return(3)
+    end
 
     subject         { decorator.available_copies }
     let(:instance)  { build :printed_book }
     let(:decorator) { described_class.new(instance) }
 
-    it { is_expected.to eql('5 copies') }
+    it { is_expected.to eql('2 copies') }
   end
 end
