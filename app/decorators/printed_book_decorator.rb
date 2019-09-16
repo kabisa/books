e4ef54366cdfdc989ac9e70c1dbefff151648e48
Patrick Baselier <patrick.baselier@gmail.com>
2019-09-16 09:17:38 +0200

    Extract view logic to decorator

--- app/decorators/printed_book_decorator.rb
+++ app/decorators/printed_book_decorator.rb
@@ -2,4 +2,38 @@ class PrintedBookDecorator < BookDecorator
   def available_copies
     h.pluralize(copies_count - borrowings_count, Copy.model_name.human.downcase)
   end
+
+  def borrow_or_return_button
+    if borrowed_by?(h.current_user)
+      h.button_tag('Return Book', class: 'btn btn-outline')
+    elsif copies.borrowables.none?
+      h.button_tag 'Borrow', class: 'btn btn-outline', disabled: true
+    elsif copies.borrowables.one?
+      h.button_to 'Borrow', h.borrowings_path(borrowing: { book_id: object, copy_id: copies.borrowables.first }), remote: true, class: 'btn btn-outline'
+    else
+      h.capture do
+        h.concat h.link_to 'Borrow...', '#', class: 'btn btn-outline', role: :button, data: { toggle: 'modal', target: "##{h.dom_id(object, :borrow)}" }
+        h.concat(h.content_tag(:div, id: h.dom_id(object, :borrow), class: 'modal fade', aria: { hidden: true, labelledby: h.dom_id(object, :label) }, role: :dialog, tabindex: -1) do
+          h.content_tag(:div, class: 'modal-dialog', role: :document) do
+            h.simple_form_for :borrowing, url: h.borrowings_path, method: :post, remote: true do |f|
+              h.concat h.hidden_field(:borrowing, :book_id, value: id)
+              h.concat(h.content_tag(:div, class: 'modal-content') do
+                h.concat(h.content_tag(:div, class: 'modal-header') do
+                  h.content_tag(:h5, "Borrow #{title}", class: 'modal-title', id: h.dom_id(object, :label))
+                end)
+                h.concat(h.content_tag(:div, class: 'modal-body') do
+                  h.concat h.content_tag(:p, 'This book is available on multiple locations.')
+                  h.concat f.input :copy_id, include_blank: false, collection: copies.borrowables, label_method: -> (c) { c.location.city }, label: 'Location', required: false
+                end)
+                h.concat(h.content_tag(:div, class: 'modal-footer') do
+                  h.concat h.link_to 'Cancel', '#', class: 'btn', role: :button, data: { dismiss: 'modal' }
+                  h.concat f.button :submit, class: 'btn-primary', data: { target: "##{h.dom_id(object, :borrow)}", toggle: 'modal' }
+                end)
+              end)
+            end
+          end
+        end)
+      end
+    end
+  end
 end
--- spec/decorators/printed_book_decorator_spec.rb
+++ spec/decorators/printed_book_decorator_spec.rb
@@ -13,4 +13,76 @@ RSpec.describe PrintedBookDecorator do
 
     it { is_expected.to eql('2 copies') }
   end
+
+  describe '#borrow_or_return_button', focus: true do
+    before { allow(decorator).to receive(:'borrowed_by?').and_return(false) }
+
+    subject         { decorator.borrow_or_return_button }
+    let(:instance)  { create :printed_book }
+    let(:decorator) { described_class.new(instance) }
+
+    context 'user is borrowing a copy' do
+    before { allow(decorator).to receive(:'borrowed_by?').and_return(true) }
+
+      it { is_expected.to have_css('button.btn.btn-outline', text: 'Return Book') }
+    end
+
+    context 'copy is not borrowable' do
+      before { allow(decorator).to receive(:copies).and_return(double('copies', borrowables: [])) }
+
+      it { is_expected.to have_css('button.btn.btn-outline[disabled="disabled"]', text: 'Borrow') }
+    end
+
+    context 'copy is available on one location' do
+      before { allow(decorator).to receive(:copies).and_return(double('copies', borrowables: [1])) }
+
+      it { is_expected.to have_css('form[method="post"][action^="/borrowings"][data-remote="true"]') }
+      it { is_expected.to have_css('form input.btn.btn-outline[value="Borrow"]') }
+    end
+
+    context 'copy is available on multiple locations' do
+      before do
+        allow(decorator).to receive(:copies).and_return(double('copies', borrowables: borrowables))
+        allow(h).to receive(:dom_id).and_return(dom_id)
+      end
+      let(:borrowables) do
+        build_stubbed_list(:copy, 2)
+      end
+      let(:dom_id) { 'dom-id' }
+
+      it { is_expected.to have_css('a.btn.btn-outline[role="button"]', text: 'Borrow...') }
+      it { is_expected.to have_css("a[data-toggle='modal'][data-target='##{dom_id}']") }
+      it { is_expected.to have_css("##{dom_id}.modal[aria-hidden='true'][role='dialog'][tabindex='-1']") }
+
+      context 'aria-labelledby' do
+        it { is_expected.to have_css("##{dom_id}.modal[aria-labelledby=#{dom_id}]") }
+        it { is_expected.to have_css(".modal h5##{dom_id}.modal-title", text: "Borrow #{instance.title}" ) }
+      end
+
+      context 'modal structure' do
+        it { is_expected.to have_css('.modal .modal-dialog[role="document"] .modal-content .modal-header') }
+        it { is_expected.to have_css('.modal .modal-dialog[role="document"] .modal-content .modal-body') }
+        it { is_expected.to have_css('.modal .modal-dialog[role="document"] .modal-content .modal-footer') }
+      end
+
+      context 'modal content' do
+        it { is_expected.to have_css('.modal form[method="post"][action^="/borrowings"][data-remote="true"] .modal-content') }
+        it { is_expected.to have_css(".modal form input[type='hidden'][name='borrowing[book_id]'][value='#{instance.id}']", visible: false) }
+
+        context 'header' do
+          it { is_expected.to have_css(".modal-header h5##{dom_id}", text: "Borrow #{instance.title}") }
+        end
+
+        context 'body' do
+          it { is_expected.to have_css('.modal-body .form-group label', text: 'Location' ) }
+          it { is_expected.to have_css('.modal-body .form-group select[name="borrowing[copy_id]"] option', count: 2 ) }
+        end
+
+        context 'footer' do
+          it { is_expected.to have_css('.modal-footer a.btn[role="button"][data-dismiss="modal"]', text: 'Cancel') }
+          it { is_expected.to have_css(".modal-footer input.btn.btn-primary[value='Borrow'][data-target='##{dom_id}'][data-toggle='modal']") }
+        end
+      end
+    end
+  end
 end
