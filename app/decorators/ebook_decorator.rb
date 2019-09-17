57285949ed981e1bfc264aac161944e708d1af54
Patrick Baselier <patrick.baselier@gmail.com>
2019-09-17 14:28:44 +0200

    Update UI

--- app/decorators/ebook_decorator.rb
+++ app/decorators/ebook_decorator.rb
@@ -1,8 +1,6 @@
 class EbookDecorator < BookDecorator
 
   def download_link
-    h.link_to(object.link, target: '_blank') do
-      h.material_icon('cloud_download', h.tooltipify(I18n.t('helpers.submit.download')))
-    end
+    h.link_to I18n.t('helpers.submit.download'), object.link, class: 'btn btn-outline', role: :button, target: '_blank'
   end
 end
