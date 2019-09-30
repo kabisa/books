5d880d9bb63b6dc9766251b5441222150aa61032
Patrick Baselier <patrick.baselier@gmail.com>
2019-09-30 11:25:34 +0200

    Improve Component

--- app/components/material/progress.rb
+++ app/components/material/progress.rb
@@ -1,12 +1,15 @@
 module Material
+  # See http://daemonite.github.io/material/docs/4.1/components/progress/
   class Progress < ActionView::Component::Base
-    def initialize(value:)
+    def initialize(value:, background: nil, html: {})
       @value = value
+      @background = background
+      @html_options = html
     end
 
     private
 
-    attr_reader :value
+    attr_reader :value, :background, :html_options
 
     def value_now
       value[:now]
@@ -24,6 +27,12 @@ module Material
       return 0 if value_max.zero?
 
       100*value_now/value_max
+    end
+
+    def progressbar_class
+      progressbar_class = %w(progress-bar)
+      progressbar_class << "bg-#{background}" if background
+      progressbar_class
     end
   end
 end
--- spec/components/material/progress_spec.rb
+++ spec/components/material/progress_spec.rb
@@ -3,23 +3,36 @@ require 'rails_helper'
 describe Material::Progress do
   subject       { Capybara.string html }
   let(:html)    {  render_inline(Material::Progress, options) }
-  let(:options) { { value: { now: 3, min: 0, max: 4 } } }
+  let(:options) { { value: value } }
+  let(:value)   { { now: 3, min: 0, max: 4 } }
 
   it { is_expected.to have_css('.progress .progress-bar[role="progressbar"]') }
   it { is_expected.to have_css('.progress-bar[aria-valuemax="4"][aria-valuemin="0"][aria-valuenow="3"]') }
   it { is_expected.to have_css('.progress-bar[style="width: 75%;"]') }
 
   context 'no min value specified' do
-    let(:options) { { value: { now: 3, max: 4 } } }
+    let(:value) { { now: 3, max: 4 } }
 
     it { is_expected.to have_css('.progress-bar[aria-valuemax="4"][aria-valuemin="0"][aria-valuenow="3"]') }
     it { is_expected.to have_css('.progress-bar[style="width: 75%;"]') }
   end
 
   context 'max value is 0' do
-    let(:options) { { value: { now: 0, max: 0 } } }
+    let(:value) { { now: 0, max: 0 } }
 
     it { is_expected.to have_css('.progress-bar[aria-valuemax="0"][aria-valuemin="0"][aria-valuenow="0"]') }
     it { is_expected.to have_css('.progress-bar[style="width: 0%;"]') }
+  end
+
+  context 'background' do
+    let(:options) { { value: value, background: :success } }
+
+    it { is_expected.to have_css('.progress-bar.bg-success') }
+  end
+
+  context 'html attributes' do
+    let(:options) { { value: value, html: { title: 'Lorem', data: { toggle: 'tooltip' }} }}
+
+    it { is_expected.to have_css('.progress[title="Lorem"][data-toggle="tooltip"]') }
   end
 end
