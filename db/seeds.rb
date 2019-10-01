40ac41efb19b9c7cf3e54cc646a882d0e1c8b162
Patrick Baselier <patrick.baselier@gmail.com>
2019-10-01 11:59:57 +0200

    Extract seed logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,77 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # bin/rails db:seed USERS_COUNT=10 BOOKS_COUNT=100
+require 'seed_builder'
 
-say("Truncate database.")
-books = Book.with_deleted
-say("Deleting #{books.count} books.")
-books.each(&:really_destroy!)
+builder = SeedBuilder.new
 
-say("Deleting users.")
-User.destroy_all
-say("Deleting locations.")
-Location.destroy_all
-
-%w(Weert Amsterdam Eindhoven).map { |city| Location.create(city: city) }
-say("Created #{Location.count} locations.")
+builder.truncate
+builder.create_locations
 
 if Rails.env.development?
-  include FactoryBot::Syntax::Methods
-
-  USER_COUNT = ENV.fetch('USERS_COUNT', 50).to_i
-  BOOKS_COUNT = ENV.fetch('BOOKS_COUNT', 200).to_i
-  EBOOKS_COUNT = BOOKS_COUNT/2
-  PRINTED_BOOKS_COUNT = BOOKS_COUNT/2
-
-  create_list(:user, USER_COUNT, :random)
-  STDOUT.puts "What's your email address?"
-  email = STDIN.gets.strip.downcase
-  current_user = User.find_or_create_by!(attributes_for(:user, :random).merge(email: email))
-
-  say("Created #{User.count} users.")
-
-  create_list(:ebook, EBOOKS_COUNT, :random)
-  say("Created #{Ebook.count} e-books.")
-
-  PRINTED_BOOKS_COUNT.times do
-    book = build(:printed_book, :random)
-    book.copies.clear
-
-    Location.all.each do |location|
-      book.copies.build(location: location, number: rand(5))
-    end
-
-    book.save
-  end
-
-  say("Created #{PrintedBook.count} printed books.")
-
-  PrintedBook.all.each do |book|
-    count = rand(book.copies.count)
-    copies = book.copies.sample(count)
-    borrowers = User.all.sample(count)
-
-    copies.each_with_index do |copy, i|
-      borrower = borrowers[i]
-      say("#{borrower} borrows #{copy}.")
-      copy.borrowings.create(user: borrower)
-    end
-  end
-
-  User.all.each do |user|
-    votes = :likes
-
-    Book.all.sample(rand(Book.count)).in_groups(2, false) do |group|
-      group.each do |book|
-        say("#{user} #{votes} #{book}")
-        book.send(votes).create(user: user)
-      end
-
-      votes = :dislikes
-    end
-  end
-
-  include Rails.application.routes.url_helpers
-  url = token_sign_in_url(current_user.login_token, host: 'localhost:3000')
-  say "Start a Rails server and navigate to #{url}."
+  builder.create_users
+  builder.create_books
+  builder.borrow_books
+  builder.vote_books
+  builder.sign_in_url
 end
---
+++ lib/seed_builder.rb
@@ -0,0 +1,120 @@
+# This class is used by `db/seeds.rb` and makes it easier
+# to understand to process in the seed.
+class SeedBuilder
+  include FactoryBot::Syntax::Methods
+  include Rails.application.routes.url_helpers
+
+  attr_reader :user_count, :books_count, :current_user
+
+  def initialize
+    @user_count = ENV.fetch('USERS_COUNT', 50).to_i
+    @books_count = ENV.fetch('BOOKS_COUNT', 200).to_i
+  end
+
+  def truncate
+    say('Truncate database.')
+    truncate_books
+    truncate_users
+    truncate_locations
+  end
+
+  def create_locations
+    %w(Weert Amsterdam Eindhoven).map { |city| Location.create(city: city) }
+    say("Created #{Location.count} locations.")
+  end
+
+  def create_users
+    create_list(:user, user_count, :random)
+    create_current_user
+    say("Created #{User.count} users.")
+  end
+
+  def create_books
+    create_ebooks
+    create_printed_books
+  end
+
+  def borrow_books
+    PrintedBook.all.each do |book|
+      count = rand(book.copies.count)
+      copies = book.copies.sample(count)
+      borrowers = User.all.reload.sample(count)
+
+      copies.each_with_index do |copy, i|
+        borrower = borrowers[i]
+        say("#{borrower} borrows #{copy}.")
+        copy.borrowings.create(user: borrower)
+      end
+    end
+  end
+
+  def vote_books
+    User.all.reload.each do |user|
+      votes = :likes
+
+      Book.all.sample(rand(Book.count)).in_groups(2, false) do |group|
+        group.each do |book|
+          say("#{user} #{votes} #{book}")
+          book.send(votes).create(user: user)
+        end
+
+        votes = :dislikes
+      end
+    end
+  end
+
+  def sign_in_url
+    url = token_sign_in_url(current_user.login_token, host: 'localhost:3000')
+    say "Start a Rails server and navigate to #{url}."
+  end
+
+  private
+
+  def ebooks_count
+    books_count/2
+  end
+
+  def truncate_books
+    books = Book.with_deleted
+    say("Deleting #{books.count} books.")
+    books.each(&:really_destroy!)
+  end
+
+  def truncate_users
+    say("Deleting users.")
+    User.destroy_all
+  end
+
+  def truncate_locations
+    say("Deleting locations.")
+    Location.destroy_all
+  end
+
+  def create_current_user
+    STDOUT.puts "What's your email address?"
+    email = STDIN.gets.strip.downcase
+    @current_user = User.find_or_create_by!(attributes_for(:user, :random).merge(email: email))
+  end
+
+  def create_ebooks
+    create_list(:ebook, ebooks_count, :random)
+    say("Created #{Ebook.count} e-books.")
+  end
+
+  def create_printed_books
+    printed_books_count.times do
+      book = build(:printed_book, :random)
+      book.copies.clear
+
+      Location.all.each do |location|
+        book.copies.build(location: location, number: rand(5))
+      end
+
+      book.save
+    end
+
+    say("Created #{PrintedBook.count} printed books.")
+  end
+
+  alias :printed_books_count :ebooks_count
+end
