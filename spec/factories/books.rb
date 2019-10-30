a56bda2e1b82ef45a5be4fc9160474ea75c8d96c
Patrick Baselier <patrick.baselier@gmail.com>
2019-10-30 11:57:52 +0100

    Update seed

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :book do
     title { 'MyString' }
     summary { 'Lorem Ipsum' }
+    tag_list { %w(Lorem Ipsum) }
 
     trait :invalid do
       title { '' }
@@ -14,7 +15,8 @@ FactoryBot.define do
       trait :random do
         title { Faker::Book.title }
         link { Faker::Internet.url }
-        summary { Array.new(rand(6)) { Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 5) }.join("\n") }
+        summary { Array.new(rand(6))  { Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 5) }.join("\n") }
+        tag_list { Array.new(rand(6)) { Faker::ProgrammingLanguage.name } }
       end
     end
 
@@ -27,7 +29,8 @@ FactoryBot.define do
 
       trait :random do
         title { Faker::Book.title }
-        summary { Array.new(rand(6)) { Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 5) }.join("\n") }
+        summary { Array.new(rand(6))  { Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 5) }.join("\n") }
+        tag_list { Array.new(rand(6)) { Faker::ProgrammingLanguage.name } }
       end
     end
   end
