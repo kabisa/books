8dffdcf9323ee08fb28920f31acacf9038f2df89
Patrick Baselier <patrick.baselier@gmail.com>
2019-10-30 09:46:22 +0100

    Rename controller

--- app/javascript/controllers/chip_input_controller.js
+++ app/javascript/controllers/chip_input_controller.js
@@ -1,6 +1,11 @@
 import {Controller} from 'stimulus';
 import Tagify from '@yaireo/tagify/dist/tagify';
 
+// This controller decorates the element with Tagify
+// to turn values in the element into chips.
+// See https://yaireo.github.io/tagify/
+// By defining a custom `tag` template we can display
+// chips as used in http://daemonite.github.io/material/docs/4.1/material/chips/
 export default class extends Controller {
   initialize() {
     const tagify = new Tagify(this.element, {
