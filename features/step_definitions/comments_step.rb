b010399d5d25f0d957438d32cffca77d51ae5be8
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-06 11:35:24 +0100

    Adjust markup

--- features/step_definitions/comments_step.rb
+++ features/step_definitions/comments_step.rb
@@ -1,3 +1,4 @@
 Then("I see {int} comments") do |comments_count|
+  expect(page).to have_content(/#{comments_count} Comments?/)
   expect(page).to have_css('.comments li', count: comments_count)
 end
