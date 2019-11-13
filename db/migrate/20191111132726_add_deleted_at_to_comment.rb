82662b123d20a9264276f2bf3dc46b0d2563f720
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-13 10:57:09 +0100

    Move migration code

--- db/migrate/20191111132726_add_deleted_at_to_comment.rb
+++ db/migrate/20191111132726_add_deleted_at_to_comment.rb
@@ -1,5 +1,10 @@
 class AddDeletedAtToComment < ActiveRecord::Migration[6.0]
   def change
+    Book.reset_column_information
+    Book.all.each do |b|
+      Book.update_counters b.id, comments_count: b.comments.length
+    end
+
     add_column :comments, :deleted_at, :datetime
     add_index :comments, :deleted_at
   end
