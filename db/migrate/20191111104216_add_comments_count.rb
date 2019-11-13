124dd3cdb3fb4791fe0bfad90c938395e518676a
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-13 11:01:55 +0100

    Fix migration

--- db/migrate/20191111104216_add_comments_count.rb
+++ db/migrate/20191111104216_add_comments_count.rb
@@ -1,5 +1,10 @@
 class AddCommentsCount < ActiveRecord::Migration[6.0]
   def change
     add_column :books, :comments_count, :integer, default: 0
+
+    Book.reset_column_information
+    Book.all.each do |b|
+      Book.update_counters b.id, comments_count: Comment.where(book: b).length
+    end
   end
 end
--- db/migrate/20191111132726_add_deleted_at_to_comment.rb
+++ db/migrate/20191111132726_add_deleted_at_to_comment.rb
@@ -1,10 +1,5 @@
 class AddDeletedAtToComment < ActiveRecord::Migration[6.0]
   def change
-    Book.reset_column_information
-    Book.all.each do |b|
-      Book.update_counters b.id, comments_count: b.comments.length
-    end
-
     add_column :comments, :deleted_at, :datetime
     add_index :comments, :deleted_at
   end
