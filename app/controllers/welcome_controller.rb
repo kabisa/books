4ce10ffffd97a85007bf3e9fe0cc01140f118392
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-14 12:08:53 +0100

    Create Suggestions model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @q = ransack_params
 
-    @last_commented_on = Comment.order(:created_at).last.book.decorate
-    @recently_added = Book.order(:created_at).last.decorate
-    @most_likes = Book.order(:likes_count).last.decorate
+    @suggestions = Suggestions.new
   end
 end
---
+++ app/models/suggestions.rb
@@ -0,0 +1,17 @@
+# This class is a WIP and currently only
+# contains 3 suggestions.
+# When there is a more relevant use-case for 'suggestions'
+# we also need to add unit tests for this class.
+class Suggestions
+  def last_commented_on
+    Comment.order(:created_at).last.book.decorate
+  end
+
+  def recently_added
+    Book.order(:created_at).last.decorate
+  end
+
+  def most_likes
+    Book.order(:likes_count).last.decorate
+  end
+end
