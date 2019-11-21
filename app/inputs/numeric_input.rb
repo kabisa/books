b9d4ea13be3ce05dc73a7478c9d414259920a975
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-21 13:39:17 +0100

    Redefine NumericInput

---
+++ app/inputs/numeric_input.rb
@@ -0,0 +1,4 @@
+require 'text_field_box'
+class NumericInput < SimpleForm::Inputs::NumericInput
+  include TextFieldBox
+end
--- config/initializers/simple_form_daemonite.rb
+++ config/initializers/simple_form_daemonite.rb
@@ -38,6 +38,23 @@ SimpleForm.setup do |config|
     end
   end
 
+  config.wrappers :inline_form_w_text_field_boxes, tag: 'span', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.wrapper tag: :div, class: 'textfield-box' do |c|
+      c.use :label, class: 'sr-only'
+
+      c.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+      c.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+      c.optional :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+    end
+  end
+
   config.wrappers :toggle_buttons, tag: :div, class: 'btn-group form-group', item_wrapper_tag: false, html: { data: { toggle: 'buttons'}, role: 'group' } do |b|
     b.use :html5
     b.optional :readonly
