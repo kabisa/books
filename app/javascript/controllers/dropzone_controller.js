dac5b04a4bb629d2b6b5fe6cd5185860b9dca930
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-25 16:09:10 +0100

    Drag 'n' drop WIP

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -1,14 +1,39 @@
 import {Controller} from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['fileInput', 'previewImage'];
-  connect() {}
+  static targets = ['fileInput', 'previewImage', 'removeButton', 'removeCover'];
+  connect() {
+    if (this.previewImageTarget.src === '') {
+      this.removeButtonTarget.style.display = 'none';
+    }
+  }
 
   handleImage(e) {
-    let reader = new FileReader();
-    reader.onload = (event) => {
-      this.previewImageTarget.setAttribute('src', event.target.result);
-    };
-    reader.readAsDataURL(e.target.files[0]);
+    let droppedFile = e.target.files[0];
+    let accepted = this.fileInputTarget.accept;
+
+    if (droppedFile.type.match(accepted)) {
+      let reader = new FileReader();
+      reader.onload = (event) => {
+        this.previewImageTarget.setAttribute('src', event.target.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+      this.removeCoverTarget.value = false;
+      this.removeButtonTarget.style.display = 'block';
+    }
+  }
+
+  browse(e) {
+    e.preventDefault();
+    this.fileInputTarget.click();
+  }
+
+  removeImage(e) {
+    e.preventDefault();
+
+    this.fileInputTarget.value = '';
+    this.removeCoverTarget.value = true;
+    this.previewImageTarget.removeAttribute('src');
+    this.removeButtonTarget.style.display = 'none';
   }
 }
