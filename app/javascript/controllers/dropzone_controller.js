1e103b19af2592112562d996163aa37befd32220
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-25 13:24:14 +0100

    Experiment w/ vanilla JS (WIP)

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -1,7 +1,14 @@
 import {Controller} from 'stimulus';
 
 export default class extends Controller {
-  connect() {
-    console.log('lorem');
+  static targets = ['fileInput', 'previewImage'];
+  connect() {}
+
+  handleImage(e) {
+    let reader = new FileReader();
+    reader.onload = (event) => {
+      this.previewImageTarget.setAttribute('src', event.target.result);
+    };
+    reader.readAsDataURL(e.target.files[0]);
   }
 }
