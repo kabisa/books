39ccfd67b55918ba206a8c69d07e28f822c13bc3
Patrick Baselier <patrick.baselier@gmail.com>
2019-11-27 11:51:28 +0100

    Extract dropzone markup

---
+++ app/inputs/dropzone_input.rb
@@ -0,0 +1,53 @@
+class DropzoneInput < SimpleForm::Inputs::FileInput
+  def input(wrapper_options)
+    input_html_options.merge!({
+      accept: 'image/*',
+      data: {
+        target: 'dropzone.fileInput',
+        action: 'dropzone#handleImage'
+      }
+    })
+    template.content_tag(:div, class: 'uploader img-thumbnail d-flex align-items-center', data: { controller: 'dropzone' }) do
+      template.concat overlay
+      template.concat image
+      template.concat super
+      template.concat remove_image
+    end
+  end
+
+
+  private
+
+  def overlay
+    template.tag.div(class: 'uploader-overlay d-flex justify-content-center align-items-center') do
+      template.concat icon1
+      template.concat icon2
+      template.concat template.tag.div(class: 'uploader-backdrop')
+    end
+
+  end
+
+  def icon1
+    template.link_to '#', class: template.sm_rnd_btn_class('uploader-action text-light'), data: { action: 'dropzone#browse' } do
+      template.material_icon('add_photo_alternate', template.tooltipify(I18n.t('upload', scope: [template.controller.controller_name, template.controller.action_name])))
+    end
+  end
+
+  def icon2
+    template.link_to '#', class: template.sm_rnd_btn_class('uploader-action ml-3 text-light'), data: { target: 'dropzone.removeButton', action: 'dropzone#removeImage' } do
+      template.material_icon('close', template.tooltipify(I18n.t('remove', scope: [template.controller.controller_name, template.controller.action_name])))
+    end
+  end
+
+  def image
+    if object.send("#{attribute_name}?")
+      template.image_tag object.send("#{attribute_name}_url"), class: 'img-fluid', data: { target: 'dropzone.previewImage' }
+    else
+      template.tag.img(class: 'img-fluid', data: { target: 'dropzone.previewImage' })
+    end
+  end
+
+  def remove_image
+    @builder.hidden_field("remove_#{attribute_name}".to_sym, data: { target: 'dropzone.removeCover' })
+  end
+end
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -24,7 +24,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.base_class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def content_type_whitelist
