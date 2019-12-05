c5f43544c68ce4e4e6c114db2ed50f68cb9956be
Patrick Baselier <patrick.baselier@gmail.com>
2019-12-05 10:54:24 +0100

    WIP

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,7 @@
+Ransack.configure do |c|
+  c.custom_arrows = {
+    up_arrow: '<i class="material-icons sort-indicator">done</i>',
+    down_arrow: '<i class="material-icons sort-indicator">done</i>',
+    default_arrow: '<span class="sort-indicator"></span>'
+  }
+end
