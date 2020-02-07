186aeba8abdb58bdbffcb78bdbb316c9c52930bc
Patrick Baselier <patrick.baselier@gmail.com>
2020-02-07 15:14:05 +0100

    Refactor

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,12 +1,3 @@
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import {Controller} from 'stimulus';
 import Autocomplete from '@trevoreyre/autocomplete-js';
 
@@ -17,44 +8,31 @@ export default class extends Controller {
     new Autocomplete(this.element, {
       search: this.search.bind(this),
       onSubmit: this.onSubmit.bind(this),
-      getResultValue: this.getResultValue.bind(this),
-      renderResult: this.renderResult.bind(this),
+      getResultValue: ({title}) => title,
+      renderResult: this.renderResult,
     });
   }
-  search(input) {
+  async search(input) {
     const url = `/books.json?q[title_cont]=${encodeURI(input)}`;
     this.valueTarget.value = '';
 
-    return new Promise((resolve) => {
-      if (input.length < 3) {
-        return resolve([]);
-      }
-
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          resolve(data);
-        });
-    });
-  }
-  onSubmit(result) {
-    this.valueTarget.value = result.id;
-  }
-  getResultValue({title}) {
-    return title;
-  }
-  renderResult(result, props) {
-    let text = `<span>${result.title}</span>`;
-
-    if (result.description) {
-      text = `${text}  - <span class="text-muted">${result.description}</span>`;
+    if (input.length < 3) {
+      return [];
     }
+
+    const response = await fetch(url);
+    return await response.json();
+  }
+  onSubmit({id}) {
+    this.valueTarget.value = id;
+  }
+  renderResult({title, description}, props) {
+    let text = title;
+
+    if (description) {
+      text = `${text} - <span class="text-muted">${description}</span>`;
+    }
+
     return `<li ${props}>${text}</li>`;
-  }
-
-  reset(e) {
-    if (e.target.value === '') {
-      this.valueTarget.value = '';
-    }
   }
 }
