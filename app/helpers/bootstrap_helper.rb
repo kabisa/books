f2c850a774c1edd1a1d16900679b9139e0b7813a
Patrick Baselier <patrick.baselier@gmail.com>
2020-05-22 10:20:03 +0200

    Add responsive_card_deck helper

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -2,7 +2,7 @@ require 'bootstrap/alert'
 
 module BootstrapHelper
   # @param icon [String] For all available icons, please refer to Material icons library (https://material.io/resources/icons/)
-  def material_icon(icon, options={})
+  def material_icon(icon, options = {})
     default_options = { class: 'material-icons' }
 
     options.deep_merge!(default_options) do |key, this_val, other_val|
@@ -14,10 +14,7 @@ module BootstrapHelper
 
   def tooltipify(title, data_attributes = {})
     data = { toggle: 'tooltip' }.merge(data_attributes)
-    {
-      data: data,
-      title: title
-    }
+    { data: data, title: title }
   end
 
   # Minimal set of classnames needed to create
@@ -29,8 +26,8 @@ module BootstrapHelper
   #   fab_class #=> 'btn-float btn'
   #   fab_class('my-1 text-primary') #=> 'btn-float btn my-1 text-primary'
   #   fab_class(%(my-1 text-primary)) #=> 'btn-float btn my-1 text-primary'
-  def fab_class(classnames=nil)
-    default_classnames = %w(btn-float btn)
+  def fab_class(classnames = nil)
+    default_classnames = %w[btn-float btn]
     join_classnames(default_classnames, classnames)
   end
 
@@ -43,13 +40,13 @@ module BootstrapHelper
   #   sm_rnd_btn_class #=> 'btn-float btn btn-sm shadow-none'
   #   sm_rnd_btn_class('my-1 text-primary') #=> 'btn-float btn btn-sm shadow-none my-1 text-primary'
   #   sm_rnd_btn_class(%(my-1 text-primary)) #=> 'btn-float btn btn-sm shadow-none my-1 text-primary'
-  def sm_rnd_btn_class(classnames=nil)
-    default_classnames = Array(fab_class(%w(btn-sm shadow-none)))
+  def sm_rnd_btn_class(classnames = nil)
+    default_classnames = Array(fab_class(%w[btn-sm shadow-none]))
     join_classnames(default_classnames, classnames)
   end
 
-  def sm_btn_class(classnames=nil)
-    default_classnames = %w(btn btn-sm)
+  def sm_btn_class(classnames = nil)
+    default_classnames = %w[btn btn-sm]
     join_classnames(default_classnames, classnames)
   end
 
@@ -57,20 +54,47 @@ module BootstrapHelper
   # @see http://daemonite.github.io/material/docs/4.1/utilities/close-icon/
   def close_icon(options = {})
     default_options = {
-      class: 'close',
-      type: :button,
-      aria: {
-        label: 'Close'
-      }
+      class: 'close', type: :button, aria: { label: 'Close' }
     }
     # Deep merge and join values
     options.deep_merge!(default_options) do |key, this_val, other_val|
       [this_val, other_val].join(' ').strip
     end
 
-    tag.button(options) do
-      tag.span('×', aria: { hidden: true })
+    tag.button(options) { tag.span('×', aria: { hidden: true }) }
+  end
+
+  def responsive_card_deck(idx)
+    capture do
+      concat wrap_every_2_on_sm(idx + 1)
+      concat wrap_every_3_on_md(idx + 1)
+      concat wrap_every_4_on_lg(idx + 1)
+      concat wrap_every_5_on_xl(idx + 1)
     end
+  end
+
+  def wrap_every_2_on_sm(tags_count)
+    return if (tags_count % 2).nonzero?
+
+    tag.div(class: 'w-100 d-none d-sm-block d-md-none')
+  end
+
+  def wrap_every_3_on_md(tags_count)
+    return if (tags_count % 3).nonzero?
+
+    tag.div(class: 'w-100 d-none d-md-block d-lg-none')
+  end
+
+  def wrap_every_4_on_lg(tags_count)
+    return if (tags_count % 4).nonzero?
+
+    tag.div(class: 'w-100 d-none d-lg-block d-xl-none')
+  end
+
+  def wrap_every_5_on_xl(tags_count)
+    return if (tags_count % 5).nonzero?
+
+    tag.div(class: 'w-100 d-none d-xl-block')
   end
 
   private
