e9c71080e03baa2ac8ace724d0d857c57ef612bb
Patrick Baselier <patrick.baselier@gmail.com>
2020-05-29 13:49:00 +0200

    Improve CardDeckBreakpointsComponent

--- app/components/bootstrap/card_deck_breakpoints_component.rb
+++ app/components/bootstrap/card_deck_breakpoints_component.rb
@@ -3,41 +3,89 @@ module Bootstrap
   # This component has no template, all markup is rendered
   # by the `call` method.
   class CardDeckBreakpointsComponent < ViewComponent::Base
-    def initialize(tag_counter:)
+    def initialize(tag_counter:, sm: 2, md: 3, lg: 4, xl: 5)
       @tag_counter = tag_counter
+      @sm = sm
+      @md = md
+      @lg = lg
+      @xl = xl
     end
 
     def call
       capture do
-        concat wrap_every_2_on_sm
-        concat wrap_every_3_on_md
-        concat wrap_every_4_on_lg
-        concat wrap_every_5_on_xl
+        concat wrap_on_sm
+        concat wrap_on_md
+        concat wrap_on_lg
+        concat wrap_on_xl
       end
     end
 
-    def wrap_every_2_on_sm
-      return if (@tag_counter % 2).nonzero?
+    private
 
-      tag.div(class: 'w-100 d-none d-sm-block d-md-none')
+    def wrap_on_sm
+      return if not_a_breakpoint?(@sm)
+
+      classnames = default_classnames << 'd-sm-block'
+      classnames << hide_on_classnames(:sm)
+
+      tag.div(class: classnames)
     end
 
-    def wrap_every_3_on_md
-      return if (@tag_counter % 3).nonzero?
+    def wrap_on_md
+      return if not_a_breakpoint?(@md)
 
-      tag.div(class: 'w-100 d-none d-md-block d-lg-none')
+      classnames = default_classnames << 'd-md-block'
+      classnames << hide_on_classnames(:md)
+
+      tag.div(class: classnames)
     end
 
-    def wrap_every_4_on_lg
-      return if (@tag_counter % 4).nonzero?
+    def wrap_on_lg
+      return if not_a_breakpoint?(@lg)
 
-      tag.div(class: 'w-100 d-none d-lg-block d-xl-none')
+      classnames = default_classnames << 'd-lg-block'
+      classnames << hide_on_classnames(:lg)
+      tag.div(class: classnames)
     end
 
-    def wrap_every_5_on_xl
-      return if (@tag_counter % 5).nonzero?
+    def wrap_on_xl
+      return if not_a_breakpoint?(@xl)
 
-      tag.div(class: 'w-100 d-none d-xl-block')
+      classnames = default_classnames << 'd-xl-block'
+      tag.div(class: classnames)
+    end
+
+    def not_a_breakpoint?(breakpoint)
+      breakpoint.nil? || (@tag_counter % breakpoint).nonzero?
+    end
+
+    def default_classnames
+      %w[w-100 d-none]
+    end
+
+    def hide_on_classnames(breakpoint)
+      [hide_on_md(breakpoint), hide_on_lg(breakpoint), hide_on_xl(breakpoint)]
+    end
+
+    def hide_on_md(breakpoint)
+      return if @md.nil?
+      return if breakpoint != :sm
+
+      'd-md-none'
+    end
+
+    def hide_on_lg(breakpoint)
+      return if @lg.nil?
+      return if %i[sm md].exclude?(breakpoint)
+
+      'd-lg-none'
+    end
+
+    def hide_on_xl(breakpoint)
+      return if @xl.nil?
+      return if %i[sm md lg].exclude?(breakpoint)
+
+      'd-xl-none'
     end
   end
 end
--- spec/components/bootstrap/card_deck_breakpoints_component_spec.rb
+++ spec/components/bootstrap/card_deck_breakpoints_component_spec.rb
@@ -5,47 +5,97 @@ describe Bootstrap::CardDeckBreakpointsComponent, type: :component do
   let(:html) { render_inline(described_class.new(options)) }
   let(:options) { { tag_counter: tag_counter } }
 
-  describe 'with 1 tag' do
-    subject { html.to_s }
-    let(:tag_counter) { 1 }
-    it { is_expected.to be_empty }
+  describe 'with default breakpoint' do
+    describe 'with 1 tag' do
+      subject { html.to_s }
+      let(:tag_counter) { 1 }
+      it { is_expected.to be_empty }
+    end
+
+    describe 'with 2 tags' do
+      let(:tag_counter) { 2 }
+      it { is_expected.to have_css('div', count: 1) }
+      it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-md-none') }
+    end
+
+    describe 'with 3 tags' do
+      let(:tag_counter) { 3 }
+      it { is_expected.to have_css('div', count: 1) }
+      it { is_expected.to have_css('.w-100.d-none.d-md-block.d-lg-none') }
+    end
+
+    describe 'with 4 tags' do
+      let(:tag_counter) { 4 }
+      it { is_expected.to have_css('div', count: 2) }
+      it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-md-none') }
+      it { is_expected.to have_css('.w-100.d-none.d-lg-block.d-xl-none') }
+    end
+
+    describe 'with 5 tags' do
+      let(:tag_counter) { 5 }
+      it { is_expected.to have_css('div', count: 1) }
+      it { is_expected.to have_css('.w-100.d-none.d-xl-block') }
+    end
+
+    describe 'with 6 tags' do
+      let(:tag_counter) { 6 }
+      it { is_expected.to have_css('div', count: 2) }
+      it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-md-none') }
+      it { is_expected.to have_css('.w-100.d-none.d-md-block.d-lg-none') }
+    end
+
+    describe 'with 7 tags' do
+      subject { html.to_s }
+      let(:tag_counter) { 7 }
+      it { is_expected.to be_empty }
+    end
   end
 
-  describe 'with 2 tags' do
-    let(:tag_counter) { 2 }
-    it { is_expected.to have_css('div', count: 1) }
-    it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-md-none') }
-  end
+  describe 'with breakpoints passed' do
+    let(:options) do
+      { tag_counter: tag_counter, sm: 3, md: nil, lg: nil, xl: 5 }
+    end
 
-  describe 'with 3 tags' do
-    let(:tag_counter) { 3 }
-    it { is_expected.to have_css('div', count: 1) }
-    it { is_expected.to have_css('.w-100.d-none.d-md-block.d-lg-none') }
-  end
+    describe 'with 1 tag' do
+      subject { html.to_s }
+      let(:tag_counter) { 1 }
+      it { is_expected.to be_empty }
+    end
 
-  describe 'with 4 tags' do
-    let(:tag_counter) { 4 }
-    it { is_expected.to have_css('div', count: 2) }
-    it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-md-none') }
-    it { is_expected.to have_css('.w-100.d-none.d-lg-block.d-xl-none') }
-  end
+    describe 'with 2 tags' do
+      subject { html.to_s }
+      let(:tag_counter) { 2 }
+      it { is_expected.to be_empty }
+    end
 
-  describe 'with 5 tags' do
-    let(:tag_counter) { 5 }
-    it { is_expected.to have_css('div', count: 1) }
-    it { is_expected.to have_css('.w-100.d-none.d-xl-block') }
-  end
+    describe 'with 3 tags' do
+      let(:tag_counter) { 3 }
+      it { is_expected.to have_css('div', count: 1) }
+      it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-xl-none') }
+    end
 
-  describe 'with 6 tags' do
-    let(:tag_counter) { 6 }
-    it { is_expected.to have_css('div', count: 2) }
-    it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-md-none') }
-    it { is_expected.to have_css('.w-100.d-none.d-md-block.d-lg-none') }
-  end
+    describe 'with 4 tags' do
+      subject { html.to_s }
+      let(:tag_counter) { 4 }
+      it { is_expected.to be_empty }
+    end
 
-  describe 'with 7 tags' do
-    subject { html.to_s }
-    let(:tag_counter) { 7 }
-    it { is_expected.to be_empty }
+    describe 'with 5 tags' do
+      let(:tag_counter) { 5 }
+      it { is_expected.to have_css('div', count: 1) }
+      it { is_expected.to have_css('.w-100.d-none.d-xl-block') }
+    end
+
+    describe 'with 6 tags' do
+      let(:tag_counter) { 6 }
+      it { is_expected.to have_css('div', count: 1) }
+      it { is_expected.to have_css('.w-100.d-none.d-sm-block.d-xl-none') }
+    end
+
+    describe 'with 7 tags' do
+      subject { html.to_s }
+      let(:tag_counter) { 7 }
+      it { is_expected.to be_empty }
+    end
   end
 end
