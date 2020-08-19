20c94830c3f717c2dd74ff6e1959874cd73acb5a
Patrick Baselier <patrick.baselier@gmail.com>
2020-08-19 11:06:45 +0200

    Try FF

--- features/support/env.rb
+++ features/support/env.rb
@@ -58,17 +58,6 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--disable-dev-shm-usage')
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-Capybara.javascript_driver = :chrome_headless
+Capybara.javascript_driver = :selenium_headless
 
 World(FactoryBot::Syntax::Methods)
